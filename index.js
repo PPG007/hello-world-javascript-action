df8f2c783e4c99f8ce0cd7b1694192c8dcd468df
Koston Zhuang <koston.zhuang@maiscrm.com>
2023-01-03 11:07:24 +0800

    v2

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const keywords = JSON.parse(core.getInput('keywords'));
+  keywords.forEach(keyword => {
+    console.log(keyword);
+  });
+  parse('.');
+  core.setOutput('result', 'success');
 } catch (error) {
   core.setFailed(error.message);
 }
+
+function parse(dirName) {
+    const files = fs.readdirSync(dirName);
+    for (let i = 0; i < files.length; i++) {
+      const tempPath = path.join(dirName, files[i]);
+      if (files[i].startsWith('.')) {
+        continue;
+      }
+      if (fs.lstatSync(tempPath).isDirectory()) {
+        parse(tempPath);
+      } else {
+        console.log(tempPath)
+      }
+    }
+  }
